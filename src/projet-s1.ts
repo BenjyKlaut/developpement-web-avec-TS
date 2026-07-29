7f60b71763c39dba46d9b6fdea7d315a737b04f4
Benjy Klaüt <benjkutart@gmail.com>
2026-07-29 10:19:33 +0100

    fix: corriger l'affichage de l'enum priorité des tâches dans la fonction afficherTache. Dans le terminal, on voit [1] au lieu de [HAUTE]. C'est parce que tache.priorite contient la valeur numérique (1), pas le nom de l'enum.

--- src/projet-s1.ts
+++ src/projet-s1.ts
@@ -49,7 +49,7 @@ function listerTache(): void {
 
 function afficherTache(tache: Tache): void {
   console.log(
-    `[#${tache.id}] "${tache.titre}" [${tache.priorite}] - Tags: ${tache.tags.join(", ")} - ${tache.terminee ? "FAITE" : "NON FAITE"} ${tache.responsable ? ` - Responsable: ${tache.responsable}` : ""}`,
+    `[#${tache.id}] "${tache.titre}" [${Priorite[tache.priorite]}] - Tags: ${tache.tags.join(", ")} - ${tache.terminee ? "FAITE" : "NON FAITE"} ${tache.responsable ? ` - Responsable: ${tache.responsable}` : ""}`,
   );
 }
 
