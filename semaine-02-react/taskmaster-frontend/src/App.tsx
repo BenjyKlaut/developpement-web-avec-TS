6bbed7ad542865434525aaeef9231a6c7fb3ea93
Benjy Klaüt <benjkutart@gmail.com>
2026-08-03 18:50:37 +0100

    refactor: restructurer le composant App  pour gérer les taches avec useState et améliorer le rendu

--- semaine-02-react/taskmaster-frontend/src/App.tsx
+++ semaine-02-react/taskmaster-frontend/src/App.tsx
@@ -1,23 +1,47 @@
-import TaskCard from './components/taskCard';
-import { type Tache, Priorite } from './types';
-import './App.css'
+import TaskCard from "./components/taskCard";
+import { type Tache, Priorite } from "./types";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const maTache: Tache = {
-    id: 1,
-    titre: "Apprendre React + TS",
-    priorite: Priorite.HAUTE,
-    terminee: false,
-    tags: ["Frontend", "Urgent"],
-    responsable: "Benjy"
-  };
+  const [taches, setTaches] = useState<Tache[]>([
+    {
+      id: 1,
+      titre: "Faire les courses",
+      priorite: Priorite.HAUTE,
+      tags: ["courses", "urgent"],
+      terminee: false,
+      responsable: "Alice",
+    },
+    {
+      id: 2,
+      titre: "Nettoyer la maison",
+      priorite: Priorite.MOYENNE,
+      tags: ["maison"],
+      terminee: true,
+    },
+    {
+      id: 3,
+      titre: "Préparer le dîner",
+      priorite: Priorite.BASSE,
+      tags: ["cuisine"],
+      terminee: false,
+      responsable: "Bob",
+    },
+  ]);
+
+  function marquerFaite(id: number) {
+    setTaches(taches.map((t) => (t.id === id ? { ...t, terminee: true } : t)));
+  }
 
   return (
-    <div>
+    <div style={{ maxWidth: "600px", margin: "0 auto", padding: "20px" }}>
       <h1>Taskmaster Pro</h1>
-      <TaskCard tache={maTache}/>
+      {taches.map((tache) => (
+        <TaskCard key={tache.id} tache={tache} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
