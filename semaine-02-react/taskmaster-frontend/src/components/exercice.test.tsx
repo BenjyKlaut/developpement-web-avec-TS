b504b7bd76cbc50836de80fe91073381501f1f67
Benjy Klaüt <benjkutart@gmail.com>
2026-08-08 11:07:46 +0100

    refactor: l'exercice était de complèter cette fonction toogleTerminee pour qu'elle inverse le statut terminee d'une tâche (si c'était false, ça devient true, et inversement).

--- semaine-02-react/taskmaster-frontend/src/components/exercice.test.tsx
+++ semaine-02-react/taskmaster-frontend/src/components/exercice.test.tsx
@@ -2,6 +2,13 @@ interface UserProps {
   nom: string;
   age: number;
 }
+/*
+function toggleTerminee(id: number) {
+  setTaches(taches.map(t => 
+    t.id === id ? { ...t, terminee: !t.terminee } : t
+  ));
+}
+*/
 
 export function UserCard({ nom, age }: UserProps) {
   return (
